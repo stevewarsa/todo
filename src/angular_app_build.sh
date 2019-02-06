baaf733e6309e072fd112f699e6f66d4fba21bb1
Steve Warsa <swarsa480@gmail.com>
2019-02-06 06:09:24 -0700

    Fix deploy script so that it removes the old main.nnnnnnn.js file, but not the main folder

--- src/angular_app_build.sh
+++ src/angular_app_build.sh
@@ -1,7 +1,7 @@
 # build UI
 cd /cygdrive/c/backup/code/Angular2/todo
 ng build --prod --base-href /todo/
-ssh swarsa_memoryverses@ssh.phx.nearlyfreespeech.net "cd /home/public/todo; ls | grep -v server | grep -v main | grep -v view | xargs rm -rf"
+ssh swarsa_memoryverses@ssh.phx.nearlyfreespeech.net "cd /home/public/todo; ls | grep -v server | grep -v '^main$' | grep -v view | xargs rm -rf"
 cd /cygdrive/c/backup/code/Angular2/todo/dist/todo
 scp -r * swarsa_memoryverses@ssh.phx.nearlyfreespeech.net:/home/public/todo
 echo "From client side, invoking /home/private/deploy-todo.sh ..."
